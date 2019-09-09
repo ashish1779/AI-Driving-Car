a76d6752f820afb90a50108dda86801ebb38d44e
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2019-09-09 15:51:40 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,140 +1,233 @@
 import RPi.GPIO as GPIO
 import time
-from time import sleep
 import csv
-
-in1 = 24 #pin 16
-in2 = 23 #pin 18
-en = 25 #pin 22
-temp1=1
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
-TRIG = 27 #pin 13
-ECHO = 22 #pin 15
+GPIO.setwarnings(False)
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
 
-print("Distance Measurement In Progress")
+A = pd.read_csv("sensor1.csv")
+B = pd.read_csv("sensor2.csv")
+C = pd.read_csv("sensor3.csv")
+X = np.hstack([A,B,C])
+y = pd.read_csv("command.csv")
 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators = 10, oob_score = True, random_state = 123456, max_depth = 10, n_jobs = -1)
+rf.fit(X,y)
+print("Done Training")
 
+#Motor Driver
+in1 = 16 #pin 36
+in2 = 19 #pin 35
+in3 = 23 #pin 16
+in4 = 24 #pin 18
+enA = 20 #pin 38
+enB = 25 #pin 22
 
+#ULtrasonic Sensors
+TRIG1 = 27 #pin 13
+ECHO1 = 22 #pin 15
+TRIG2 = 5 #pin29
+ECHO2 = 6 #pin31
+TRIG3 = 17 #pin11
+ECHO3 = 26 #pin37
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(TRIG1,GPIO.OUT)
+GPIO.setup(ECHO1,GPIO.IN)
+GPIO.setup(TRIG2,GPIO.OUT)
+GPIO.setup(ECHO2,GPIO.IN)
+GPIO.setup(TRIG3,GPIO.OUT)
+GPIO.setup(ECHO3,GPIO.IN)
+ 
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
+GPIO.setup(enA,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-
-
-
-p=GPIO.PWM(en,1000)
-p.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
-
-while(1):
-
-    x=raw_input()
-    
-    if x=='r':
-        print("run")
-        if(temp1==1):
-            GPIO.output(in1,GPIO.HIGH)
-            GPIO.output(in2,GPIO.LOW)
-            print("forward")
-            x='z'
-            time.sleep(5)
-            GPIO.output(TRIG, False)
-            print("Waiting For Sensor To Settle")
-            time.sleep(2)
-            GPIO.output(TRIG, True)
-            time.sleep(0.00001)
-            GPIO.output(TRIG, False)
-            while GPIO.input(ECHO)==0:
-                pulse_start = time.time()
-            while GPIO.input(ECHO)==1:
-                pulse_end = time.time()
-            pulse_duration = pulse_end - pulse_start
  
-            distance = pulse_duration * 17150
  
-            distance = round(distance, 2)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(enB,GPIO.OUT)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
  
-            print("Distance:",distance,"cm")
-            GPIO.cleanup()
+p1 = GPIO.PWM(enA,1000)
+p2 = GPIO.PWM(enB,1000)
 
 
-         
-        else:
-            GPIO.output(in1,GPIO.LOW)
-            GPIO.output(in2,GPIO.HIGH)
-            print("backward")
-            x='z'
+def distance1():
+
+    GPIO.output(TRIG1, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG1, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    while GPIO.input(ECHO1) == 0:
+        StartTime = time.time()
+
+    while GPIO.input(ECHO1) == 1:
+        StopTime = time.time()
+
+    TimeElapsed = StopTime - StartTime
+    distance1 = int((TimeElapsed * 34300) / 2)
+    return distance1
 
 
-    elif x=='s':
-        print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        x='z'
+def distance2():
 
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
+    GPIO.output(TRIG2, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG2, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
 
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
+    while GPIO.input(ECHO2) == 0:
+        StartTime = time.time()
 
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        x='z'
+    while GPIO.input(ECHO2) == 1:
+        StopTime = time.time()
 
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        x='z'
-     
-    
-    elif x=='e':
-        GPIO.cleanup()
-        break
-    
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+    TimeElapsed = StopTime - StartTime
+    distance2 = int((TimeElapsed * 34300) / 2)
+    return distance2
 
 
+def distance3():
 
+    GPIO.output(TRIG3, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG3, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    while GPIO.input(ECHO3) == 0:
+        StartTime = time.time()
 
+    while GPIO.input(ECHO3) == 1:
+        StopTime = time.time()
 
+    TimeElapsed = StopTime - StartTime
+    distance3 = int((TimeElapsed * 34300) / 2)
+    return distance3
 
+       
+if __name__ == '__main__':
+    try:
+        while True:
+            sensor1 = distance1()
+            sensor2 = distance2()
+            sensor3 = distance3()
+            if (sensor3 <= 2):
+                p1.start(50)
+		p2.start(50)
 
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in1,GPIO.HIGH)
+                GPIO.output(in3,GPIO.LOW)
+                GPIO.output(in4,GPIO.HIGH)
+                print("backward")
 
+            if (sensor1 <= 4):
+                p1.start(35)
+		p2.start(45)
+                 
+                GPIO.output(in2,GPIO.HIGH)
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in4,GPIO.HIGH)
+                GPIO.output(in3,GPIO.LOW)
+                print("left")
+
+            if (sensor2 <= 4):
+                p1.start(45)
+		p2.start(35)
+
+                GPIO.output(in2,GPIO.LOW)
+     	        GPIO.output(in1,GPIO.HIGH)
+                GPIO.output(in3,GPIO.HIGH)
+                GPIO.output(in4,GPIO.LOW)
+                print("right")
+		
+	    print([sensor3,sensor2,sensor1])
+
+            yolo = np.array([sensor3,sensor2,sensor1])
+            yolo.reshape(3,1)
+            new = np.reshape(yolo, (1, 3))
+            new = scaler.transform(new)
+            ytest = rf.predict(new)
+            FLAG = int(ytest[0])
+
+            if FLAG == 1: #Forward
+		p1.start(45)
+		p2.start(45)
+
+                print("forward")
+                GPIO.output(in1,GPIO.HIGH)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in4,GPIO.HIGH)
+       	        GPIO.output(in3,GPIO.LOW)
+            
+   
+            elif FLAG == 2: #Right
+		p1.start(45)
+		p2.start(35)
+
+		print("right")
+                GPIO.output(in2,GPIO.LOW)
+     	        GPIO.output(in1,GPIO.HIGH)
+                GPIO.output(in3,GPIO.HIGH)
+                GPIO.output(in4,GPIO.LOW)
+            
+
+            elif FLAG == 3: #Left
+		p1.start(35)
+		p2.start(45)
+
+	        print("left")
+                GPIO.output(in2,GPIO.HIGH)
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in4,GPIO.HIGH)
+                GPIO.output(in3,GPIO.LOW)
+                
+
+            elif FLAG == 4: #Backward
+		p1.start(50)
+		p2.start(50)
+		
+		print("backward")
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in2,GPIO.HIGH)
+                GPIO.output(in4,GPIO.LOW)
+                GPIO.output(in3,GPIO.HIGH)
+
+            print(yolo)
  
 
- 
+    except:
+	GPIO.output(in1, GPIO.LOW)
+	GPIO.output(in2, GPIO.LOW)
+	GPIO.output(in3, GPIO.LOW)
+	GPIO.output(in4, GPIO.LOW)
+	GPIO.output(enB, GPIO.LOW) 
+	GPIO.output(enA, GPIO.LOW)
 
- 
+	GPIO.setup(TRIG1,GPIO.LOW)
+	GPIO.setup(TRIG2,GPIO.LOW)
+	GPIO.setup(TRIG3,GPIO.LOW)
 
-
- 
-
- 
-
- 
-
+	GPIO.setup(ECHO1,GPIO.LOW)
+	GPIO.setup(ECHO2,GPIO.LOW)
+	GPIO.setup(ECHO3,GPIO.LOW)
+        pass
